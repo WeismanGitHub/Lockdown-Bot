8a663dd46058bc9ee591650b68c46df1a9f26507
weisman <weisman123@outlook.com>
2024-03-22 15:12:36 -0700

    Setup some boilerplate

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,19 +1,17 @@
 using DSharpPlus;
-using DSharpPlus.SlashCommands;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration;
+
+//using DSharpPlus.SlashCommands;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration.Get<Configuration>()!;
 
 builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton(config);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,26 +19,19 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.Run();
-IConfiguration config = new ConfigurationBuilder().AddUserSecrets<Configuration>().Build();
-
-var token = config.GetSection("Token").Value;
 
 var discord = new DiscordClient(
     new DiscordConfiguration()
     {
-        Token = token,
+        Token = config.Token,
         TokenType = TokenType.Bot,
         Intents = DiscordIntents.Guilds
     }
 );
 
-var slash = discord.UseSlashCommands();
+//var slash = discord.UseSlashCommands();
 
 await discord.ConnectAsync();
-await Task.Delay(-1);
+app.Run();
