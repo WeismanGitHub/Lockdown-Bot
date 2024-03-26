7586899f71ecb3ea8e97041f669103ca24c25a15
weisman <weisman123@outlook.com>
2024-03-26 00:40:43 -0700

    Setup first command

---
+++ Server/SlashCommands.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace Server;
+
+public class MessageAnalyticsCommands : ApplicationCommandModule
+{
+    [SlashCommand("messages", "Count the total amount of messages.")]
+    public async Task Messages(InteractionContext ctx)
+    {
+        Console.WriteLine(ctx.Guild.Name);
+        await ctx.CreateResponseAsync("hello world!");
+    }
+}
