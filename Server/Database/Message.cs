5ebfae587e647c0217938cb3c3ee693b47a20d15
weisman <weisman123@outlook.com>
2024-03-29 03:24:21 -0700

    Create Message class

---
+++ Server/Database/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Database;
+
+public class Message
+{
+    public string UserId { get; set; }
+    public string MessageId { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
