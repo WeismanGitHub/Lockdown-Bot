31b8b42214e181b4480f216e7aef80e79628458d
weisman <weisman123@outlook.com>
2024-03-29 03:24:13 -0700

    Create dbContext

---
+++ Server/Database/dbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Server.Database;
+
+using System;
+using Microsoft.EntityFrameworkCore;
+
+public class AnalyticsContext : DbContext
+{
+    public DbSet<Message> Message { get; set; }
+
+    public string DbPath { get; }
+
+    public AnalyticsContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "analytics.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options.UseSqlite($"Data Source={DbPath}");
+}
