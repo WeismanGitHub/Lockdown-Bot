a0d32c4492dadeb142169d2814eae4e9cd22932c
weisman <weisman123@outlook.com>
2024-03-29 23:48:55 -0700

    Create VoiceCall schema

---
+++ Server/Database/VoiceCall.cs
@@ -0,0 +1,10 @@
+﻿namespace Server.Database;
+
+public class VoiceCall
+{
+    public required string ChannelId { get; set; }
+    public required string GuildId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+}
+// figure out if/how you want to store users. maybe do it individually per person.
