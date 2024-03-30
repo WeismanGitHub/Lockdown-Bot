7cf0dcf6dc044422b412baca37e084b1d0f62386
weisman <weisman123@outlook.com>
2024-03-30 00:03:35 -0700

    Define relationships between tables

--- Server/Database/dbContext.cs
+++ Server/Database/dbContext.cs
@@ -18,6 +18,12 @@ public class AnalyticsContext : DbContext
         DbPath = Path.Join(path, "analytics.db");
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Guild>().HasMany(g => g.Messages).WithOne(m => m.Guild);
+        modelBuilder.Entity<Guild>().HasMany(g => g.VoiceCalls).WithOne(vc => vc.Guild);
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder options) =>
         options.UseSqlite($"Data Source={DbPath}");
 }
