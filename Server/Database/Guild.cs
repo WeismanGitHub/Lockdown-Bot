41a2e341680c232f4ec781dc1d09a4ec16fcac37
weisman <weisman123@outlook.com>
2024-03-29 23:48:39 -0700

    Create Guild schema

---
+++ Server/Database/Guild.cs
@@ -0,0 +1,14 @@
+﻿namespace Server.Database;
+
+public enum Visibility
+{
+    Members,
+    Admins
+}
+
+public class Guild
+{
+    public required string GuildId { get; set; }
+    public Visibility Visibility { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
