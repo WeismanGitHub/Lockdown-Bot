7187123aeb3c5eed443b0d585d9ec9ba80d1ad62
weisman <weisman123@outlook.com>
2024-03-30 22:36:04 -0700

    Migrate db

--- Server/Migrations/20240331053403_CreateTables.Designer.cs
+++ Server/Migrations/20240331053403_CreateTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Server.Database;
 
@@ -10,9 +11,11 @@ using Server.Database;
 namespace Server.Migrations
 {
     [DbContext(typeof(AnalyticsContext))]
-    partial class AnalyticsContextModelSnapshot : ModelSnapshot
+    [Migration("20240331053403_CreateTables")]
+    partial class CreateTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
