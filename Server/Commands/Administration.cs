b17810c2cbf2e1dcd636c4c6d6246e49fed8b077
weisman <weisman123@outlook.com>
2024-04-04 01:15:49 -0700

    Create command outline

--- Server/Commands/Administration.cs
+++ Server/Commands/Administration.cs
@@ -1,13 +1,41 @@
 ﻿namespace Server.Commands;
 
+using System.Runtime.InteropServices;
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using Server.Database;
 
 public class AdministrationCommands : ApplicationCommandModule
 {
+    public GuildService GuildService { private get; set; } // Implied public setter.
+
     [SlashCommand("settings", "Modify the settings.")]
-    public async Task Settings(InteractionContext ctx)
+    public async Task Settings(
+        InteractionContext ctx,
+        [Option("Visibility", "Decide who can see server analytics.")] Visibility? visibility = null
+    )
     {
-        Console.WriteLine(ctx.Guild.Name);
-        await ctx.CreateResponseAsync("hello world!");
+        Guild? guild;
+
+        if (visibility != null)
+        {
+            guild = new Guild()
+            {
+                GuildId = ctx.Guild.Id.ToString(),
+                Visibility = (Visibility)visibility
+            };
+
+            await GuildService.UpsertGuild(guild);
+        }
+        else
+        {
+            guild = await GuildService.GetGuild(ctx.Guild.Id.ToString());
+        }
+
+        await ctx.CreateResponseAsync(
+            new DiscordEmbedBuilder()
+                .WithTitle($"Settings for ${ctx.Guild.Name}")
+                .AddField("Visibility", guild?.Visibility.ToString() ?? "Unknown")
+        );
     }
 }
