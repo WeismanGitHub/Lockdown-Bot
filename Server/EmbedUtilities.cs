b3e5579bef509087e3e5202e1fffbf85c354e3bb
weisman <weisman123@outlook.com>
2024-04-04 22:48:42 -0700

    Add method to create error embeds

--- Server/EmbedUtilities.cs
+++ Server/EmbedUtilities.cs
@@ -8,4 +8,12 @@ public static class EmbedUtilities
     {
         return new DiscordEmbedBuilder().WithColor(new DiscordColor("cccccc"));
     }
+
+    public static DiscordEmbedBuilder CreateErrorEmbed(string? description)
+    {
+        return new DiscordEmbedBuilder()
+            .WithColor(new DiscordColor("FF0000"))
+            .WithTitle("An Error Occurred!")
+            .WithDescription(description);
+    }
 }
