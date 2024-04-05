1ffae88d1fa5678cf003986f93f5403624337fb5
weisman <weisman123@outlook.com>
2024-04-04 22:52:27 -0700

    Add Red/White color properties

--- Server/EmbedUtilities.cs
+++ Server/EmbedUtilities.cs
@@ -4,15 +4,18 @@ namespace Server;
 
 public static class EmbedUtilities
 {
+    public static readonly DiscordColor White = new("cccccc");
+    public static readonly DiscordColor Red = new("FF0000");
+
     public static DiscordEmbedBuilder CreateBuilder()
     {
-        return new DiscordEmbedBuilder().WithColor(new DiscordColor("cccccc"));
+        return new DiscordEmbedBuilder().WithColor(White);
     }
 
     public static DiscordEmbedBuilder CreateErrorEmbed(string? description)
     {
         return new DiscordEmbedBuilder()
-            .WithColor(new DiscordColor("FF0000"))
+            .WithColor(Red)
             .WithTitle("An Error Occurred!")
             .WithDescription(description);
     }
