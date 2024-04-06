6c82163257e7da0aa079c8fd4ae63970cef4dc49
weisman <weisman123@outlook.com>
2024-04-06 12:40:31 -0700

    Create Extensions class

---
+++ Server/Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Server.Extensions;
+
+using DSharpPlus.Entities;
+using Server.Database;
+
+public static class Extensions
+{
+    public static IEnumerable<Message> Convert(this IEnumerable<DiscordMessage> messages)
+    {
+        return messages.Select(msg => new Message()
+        {
+            Bot = msg.Author.IsBot,
+            GuildId = msg.Channel.Guild.Id.ToString(),
+            CreatedAt = msg.CreationTimestamp,
+            MessageId = msg.Id.ToString(),
+            TextLength = msg.Content.Length,
+            UserId = msg.Author.Id.ToString()
+        });
+    }
+}
