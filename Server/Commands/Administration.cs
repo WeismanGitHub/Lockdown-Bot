57c6e59efc9b9850f107cdcf5aca000f28cd0e74
weisman <weisman123@outlook.com>
2024-04-06 05:10:27 -0700

    Finish command

--- Server/Commands/Administration.cs
+++ Server/Commands/Administration.cs
@@ -9,16 +9,16 @@ public class AdministrationCommands : ApplicationCommandModule
 {
     public required GuildService GuildService { private get; set; }
 
-    [SlashCommand("settings", "Modify the settings."), GuildOnly]
+    [SlashCommand("Settings", "Modify the settings."), GuildOnly]
     public async Task Settings(
         InteractionContext ctx,
         [Option("Visibility", "Decide who can see server analytics.")]
             Visibility? visibility = null,
-        [Option("Viewer Role", "Set a viewer role.")] DiscordRole? viewerRole = null
+        [Option("Viewer-Role", "Set a viewer role.")] DiscordRole? viewerRole = null
     )
     {
         var guildId = ctx.Guild.Id.ToString();
-        Guild guild = await GuildService.GetGuild(guildId) ?? new Guild() { GuildId = guildId };
+        var guild = await GuildService.GetGuild(guildId);
 
         var changes = visibility != null || viewerRole != null;
         var hasPermissions =
@@ -27,16 +27,26 @@ public class AdministrationCommands : ApplicationCommandModule
 
         if (hasPermissions && changes)
         {
+            guild ??= new Guild() { GuildId = guildId };
+
             guild.Visibility = visibility ?? guild.Visibility;
             guild.ViewerRoleId = viewerRole?.Id.ToString();
 
             await GuildService.UpsertGuild(guild);
         }
 
+        if (guild == null)
+        {
+            await ctx.CreateResponseAsync(
+                EmbedUtilities.CreateErrorEmbed("This server has not been set up yet.")
+            );
+            return;
+        }
+
         await ctx.CreateResponseAsync(
             EmbedUtilities
                 .CreateBuilder()
-                .WithTitle($"Settings for ${ctx.Guild.Name}")
+                .WithTitle($"Settings for {ctx.Guild.Name}")
                 .AddField("Visibility", guild?.Visibility.ToString() ?? "Unknown")
         );
     }
