d6553fc79da288a6e95ae5fb876ffded71b87428
weisman <weisman123@outlook.com>
2024-04-09 03:02:17 -0700

    Create class to register events/commands

---
+++ Server/Bot/ClientUtilities.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using DSharpPlus.SlashCommands;
+using Server.Bot.Events;
+
+namespace Server.Bot;
+
+public class ClientUtilities
+{
+    private readonly SlashCommandsExtension slash;
+    private readonly DiscordClient client;
+
+    public ClientUtilities(SlashCommandsExtension slash, DiscordClient client)
+    {
+        this.slash = slash;
+        this.client = client;
+    }
+
+    public ClientUtilities RegisterCommands()
+    {
+        var commands = Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => t.Namespace != null && t.Namespace.StartsWith("Server.Bot.Commands"))
+            .Where(s => s.Name.Contains("Commands"));
+
+        foreach (var command in commands)
+        {
+            slash.RegisterCommands(command);
+        }
+
+        return this;
+    }
+
+    public ClientUtilities RegisterEvents()
+    {
+        client.MessageCreated += MessageCreated.Handler;
+        slash.SlashCommandErrored += SlashCommandErrored.Handler;
+
+        return this;
+    }
+}
