dcf9116b63e03dd5b4106ae24838da2e806adcb1
weisman <weisman123@outlook.com>
2024-04-10 11:52:13 -0700

    Create event error handling class

---
+++ Server/Bot/Events/ClientErrored.cs
@@ -0,0 +1,17 @@
+﻿namespace Server.Bot.Events;
+
+using DSharpPlus.EventArgs;
+using Server.Bot;
+
+public static class ClientErrored
+{
+    public static async Task Handler(DiscordClient _, ClientErrorEventArgs e)
+    {
+        var errorEmbed = EmbedUtilities.CreateErrorEmbed(e.Exception);
+        Console.WriteLine(errorEmbed);
+
+        //if (e.Handled)
+
+        //      await e.Context.CreateResponseAsync(errorEmbed);
+    }
+}
