dc13440223ed8637531cba64276cf2620f0df403
weisman <weisman123@outlook.com>
2024-04-12 16:17:15 -0700

    Outline basically the whole handler

--- Server/Bot/Events/MessageCreated.cs
+++ Server/Bot/Events/MessageCreated.cs
@@ -1,11 +1,22 @@
 ﻿namespace Server.Bot.Events;
 
 using DSharpPlus.EventArgs;
+using Server.Database;
 
 public static class MessageCreated
 {
     public static async Task Handler(DiscordClient _, MessageCreateEventArgs e)
     {
-        Console.WriteLine(e.Author.Username);
+        // Guild and Author are actually nullable.
+        if (e.Guild is null || e.Author is null)
+        {
+            return;
+        }
+
+        // Check cache to see if we want to record messages for this guild.
+
+        var messageService = new MessageService();
+
+        await messageService.InsertMessage(e.Message.Convert());
     }
 }
